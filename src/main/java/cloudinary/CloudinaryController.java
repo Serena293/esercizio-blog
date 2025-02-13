c41179a0951db96759f099d88802b42553f44f83
SERENA FERRARO <serenaferraro21@gmail.com>
2025-02-13 15:55:17 +0000

    update

---
+++ src/main/java/cloudinary/CloudinaryController.java
@@ -0,0 +1,39 @@
+package cloudinary;
+
+import com.cloudinary.Cloudinary;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/images")
+@RequiredArgsConstructor
+public class CloudinaryController {
+    private final Cloudinary cloudinary;
+
+    @PostMapping(path="/uploadme", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void upload(
+            @RequestPart("file")
+            MultipartFile file) {
+
+        try {
+                        Map result = cloudinary.uploader()
+                    .upload(file.getBytes(),  Cloudinary.asMap("folder", "FS0824", "public_id", file.getOriginalFilename()));
+
+
+            String url = result.get("secure_url").toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
